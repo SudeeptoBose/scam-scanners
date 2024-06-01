a09a0eef5dd69e497e2303d3de17218d2c90c48e
SudeeptoBose <sudeeptobose23@gmail.com>
2024-06-02 00:35:04 +0600

    edits

--- src/pages/Meme.jsx
+++ src/pages/Meme.jsx
@@ -7,7 +7,7 @@ const Meme = () => {
 
   return (
 	<div className='bg-[#efea69] p-10 w-full'>
-		<div className="embla mx-auto mt-12 h-auto max-w-lg border" ref={emblaRef}>
+		<div className="embla mx-auto mt-12 h-auto max-w-lg border-black" ref={emblaRef}>
 		<div className="embla__container h-full">
 			<div className="embla__slide flex items-center justify-center">
 				<img src='meme1.png' className='rounded-md mb-2 w-full'/>
